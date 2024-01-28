a5270b31c15e10e571c8ca265f39b6df69c3feca
majedop <128166712+majedop@users.noreply.github.com>
2024-01-28 23:27:32 +0600

    Create Class11_2.cpp

---
+++ Class11_2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mysum(int a, int b)
+{
+  int ans = a + b;
+  return ans; // return ans to main function = 50
+}
+
+int main()
+{
+  int ans = mysum(20, 30); // function call
+  cout << ans;
+
+  return 0;
+}
